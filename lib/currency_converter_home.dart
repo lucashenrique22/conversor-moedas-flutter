48c6434a7d9db4f237ab6a4ba2f0d1bec7fde339
lucashenrique22 <lh834392@gmail.com>
2025-06-19 17:01:00 -0300

    criando input para conversao

--- lib/currency_converter_home.dart
+++ lib/currency_converter_home.dart
@@ -8,11 +8,62 @@ class CurrencyConverterHome extends StatefulWidget {
 }
 
 class _CurrencyConverterHomeState extends State<CurrencyConverterHome> {
+  String fromCurrency = 'USD';
+  String toCurrency = 'EUR';
+  double amount = 0.0;
+  double rate = 0.0;
+  double total = 0.0;
+  TextEditingController amountController = TextEditingController();
+  List<String> currencies = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Conversor de Moedas'))
+      backgroundColor: Color(0xFF1d2630),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.white,
+        elevation: 0,
+        title: Text('Currency Converter'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+                child: TextField(
+                  controller: amountController,
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: 'Amount',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      amount = double.tryParse(value) ?? 0.0;
+                      total = amount * rate;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
